7397d5a860e4a169c3d2fb7fa9b9517444bf356c
souravrs999 <souravraveendran6@gmail.com>
2021-07-12 08:49:55 +0530

    verification

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,17 @@ import Head from "next/head";
 export default function HeaderModule() {
   return (
     <>
-      <meta name="propeller" content="e85f01f028b7c9713bc593791644475b" />
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="description" content="" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="propeller" content="e85f01f028b7c9713bc593791644475b" />
+        <title>AD BLASTER META</title>
+      </Head>
     </>
   );
 }
